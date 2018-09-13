2df9f39c14f18ba1e64f0f450e92bf0a3bea45ed
Shen Qu <53157020@qq.com>
2018-09-13 10:56:49 +1000

    upload lg file path

---
+++ bfs/lengthLongestPath.java
@@ -0,0 +1,12 @@
+public class Solution{
+    public int lengthLongestPath(String input){
+
+        if(input.length() == 0){
+            return 0;
+        }
+
+        String[] strs = input.split("\n")
+    }
+
+
+}
