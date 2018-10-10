f346174934bb9b5a5a8261f570b5ff4081c32afc
Shen Qu <53157020@qq.com>
2018-10-10 16:52:54 +0800

    tricks

---
+++ tag/google/lengthOfLongestSubstring.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        dic, res, start, = {}, 0, 0
+        for i, ch in enumerate(s):
+            if ch in dic:
+                res = max(res, i - start)
+                start = max(start, dic[ch] + 1)
+            dic[ch] = i
+        return max(res, len(s) - start)
+
+
+l = Solution()
+l.lengthOfLongestSubstring("abcabcbb")
