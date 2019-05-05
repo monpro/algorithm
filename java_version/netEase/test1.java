389ef5e679e17808245e33e07a90f37c84cfe82b
manny.qu <q53157020@gmail.com>
2019-05-05 12:04:17 +1000

    upload some solution to binary tree

--- java_version/netEase/test1.java
+++
@@ -1,8 +0,0 @@
-package netEase;
-
-public class test1 {
-    public static void main(String[] args){
-        System.out.println("good");
-    }
-
-}
---
+++ java_version/tree/ConstruBinaryTreeFromInorderPostorder_106.java
@@ -0,0 +1,7 @@
+package tree;
+
+public class ConstruBinaryTreeFromInorderPostorder_106 {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return null;
+    }
+}
