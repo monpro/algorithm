85f5ad29f40da03e6887e25e1049fc68b5553346
monpro <monproshen@gmail.com>
2019-07-28 12:50:52 +1000

    warm up for dp

--- java_version/bfs/DirectedGraphNode.java
+++
@@ -1,8 +0,0 @@
-package bfs;
-
-import java.util.ArrayList;
-class DirectedGraphNode {
-    public int label;
-    public ArrayList<DirectedGraphNode> neighbors;
-    DirectedGraphNode(int x) { label = x; neighbors = new ArrayList<DirectedGraphNode>(); }
-};
--- java_version/tree/TreeNode.java
+++
@@ -1,8 +0,0 @@
-package tree;
-
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x){val = x;}
-}
