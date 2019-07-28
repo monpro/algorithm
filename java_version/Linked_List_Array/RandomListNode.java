513b1c21ba114a8ac5e2f204e2459820e529a643
monpro <monproshen@gmail.com>
2019-07-28 12:50:52 +1000

    warm up for dp

--- java_version/Linked_List_Array/RandomListNode.java
+++
@@ -1,9 +0,0 @@
-package Linked_List_Array;
-
-public class RandomListNode {
-    int label;
-    RandomListNode next, random;
-    RandomListNode(int x){
-        this.label = x;
-    }
-}
--- java_version/double_pointer/SumCloset_16.java
+++
@@ -1,7 +0,0 @@
-package double_pointer;
-
-public class SumCloset_16 {
-    public int threeSumClosest(int[] nums, int target) {
-        return 0;
-    }
-}
