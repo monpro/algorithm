c58138e5ba108edfeaaff0a06c01118310ddb853
monpro <monproshen@gmail.com>
2019-08-07 15:40:20 +1000

    add several graph problems

--- python_version/graphs/course-schedule-210.py
+++ python_version/graphs/course-schedule-210.py
@@ -1,60 +1,38 @@
-class Solution:
-    """
-    @param: numCourses: a total of n courses
-    @param: prerequisites: a list of prerequisite pairs
-    @return: the course order
-    """
-
+class Solution(object):
     def findOrder(self, numCourses, prerequisites):
         # write your code here
         result = []
-        if prerequisites == []:
+        if not prerequisites:
             for i in range(numCourses):
                 result.append(i)
             return result
-
         # create indegree_hash_map and node_set
         node_set = set()
         queue = []
-
         for i in prerequisites:
             for j in i:
                 node_set.add(j)
-
         indegree_hash_map = dict((i, 0) for i in node_set)
         neighbours = dict((i, []) for i in node_set)
-
         for i in prerequisites:
             neighbours[i[1]].append(i[0])
             indegree_hash_map[i[0]] += 1
         for i in indegree_hash_map:
             if indegree_hash_map[i] == 0:
                 queue.append(i)
-
         count = 0
-        while queue != []:
+        while queue:
             e = queue.pop(0)
             result.append(e)
             for i in neighbours[e]:
                 indegree_hash_map[i] -= 1
                 if indegree_hash_map[i] == 0:
                     queue.append(i)
-
         length = len(result)
-
         if length == len(node_set):
             for i in range(numCourses):
                 if i not in node_set:
                     result.append(i)
-
             return result
         else:
             return []
-
-
-
-
-
-
-
-
