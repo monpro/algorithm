e50a3f3103c0db670949e28a0573b0a4b39155ba
monpro <monproshen@gmail.com>
2019-08-08 16:50:44 +1000

    add some dp tricks

--- python_version/dps/unique-paths-62.py
+++ python_version/dps/unique-paths-62.py
@@ -13,8 +13,23 @@ class Solution(object):
         for i in range(1, m):
             for j in range(1, n):
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
-
         return dp[m - 1][n - 1]
+
+    def uniquePaths(self, m, n):
+        '''
+        :param m:
+        :param n:
+        :return:
+        '''
+        prev = [1 for i in range(m)]
+        cur = [1 for i in range(m)]
+        for i in range(1,n):
+            for j in range(1, m):
+                cur[j] = prev[j] + cur[j - 1]
+                prev = cur
+        return cur[m - 1]
+
+
 if __name__ == "__main__":
     l = Solution()
     print(l.uniquePaths(7,3))
