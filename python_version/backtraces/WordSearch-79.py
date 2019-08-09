77c601eb6b0ad4d49647e8e0f20d6e41168a5342
monpro <monproshen@gmail.com>
2019-08-09 16:02:24 +1000

    use tricks to resolve search in rotated array

--- python_version/backtraces/WordSearch-79.py
+++ python_version/backtraces/WordSearch-79.py
@@ -4,31 +4,31 @@ class Solution:
     @param word: A string
     @return: A boolean
     """
+
     def exist(self, board, word):
         if len(word) == 0:
             return False
         if len(board) == 0 or len(board[0]) == 0:
             return False
-        
+
         for i in range(len(board)):
             for j in range(len(board[i])):
                 if board[i][j] == word[0]:
                     if self.helper(board, word, i, j, 0):
                         return True
         return False
-    
+
     def helper(self, board, word, row, col, index):
         if index == len(word):
             return True
         if row == len(board) or col == len(board[0]) or row < 0 or col < 0 or board[row][col] != word[index]:
             return False
-    
+
         board[row][col] = "-1"
-        directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         result = False
         for direction in directions:
             if self.helper(board, word, row + direction[0], col + direction[1], index + 1):
-                result = True
-                break
+                return True
         board[row][col] = word[index]
         return result
