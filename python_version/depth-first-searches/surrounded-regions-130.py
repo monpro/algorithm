ee772a8ec543da3766d7ed003d47a704cec8dc4d
monpro <monproshen@gmail.com>
2019-08-27 17:24:34 +1000

    add array tricks

--- python_version/depth-first-searches/surrounded-regions-130.py
+++ python_version/depth-first-searches/surrounded-regions-130.py
@@ -4,8 +4,37 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         # use bfs to flood the board
-        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-        for direction in directions:
-          print(direction)
-          
+        if not board or not board[0]:
+            return
+        rowLimit = len(board)
+        columnLimit = len(board[0])
+        for i in range(columnLimit):
+          if board[0][i] == 'O':
+            self.dfs(board, 0, i)
+          if board[rowLimit - 1][i] == 'O':
+            self.dfs(board, rowLimit - 1, i)
         
+        for j in range(rowLimit):
+          if board[j][0] == 'O':
+            self.dfs(board, j, 0)
+          if board[j][columnLimit - 1]:
+            self.dfs(board, j, columnLimit - 1)
+
+        for i in range(rowLimit):
+          for j in range(columnLimit):
+            if board[i][j] == 'O':
+              board[i][j] = 'X'
+            if board[i][j] == '*':
+              board[i][j] = 'O'
+    def dfs(self, board, row, column):
+      rowLimit = len(board)
+      columnLimit = len(board[0])
+      if row < 0 or row >= rowLimit or column < 0 or column >= columnLimit:
+        return
+      if board[row][column] == 'X' or board[row][column] == '*':
+        return 
+      board[row][column] = '*'
+      self.dfs(board, row + 1, column)
+      self.dfs(board, row - 1, column)
+      self.dfs(board, row, column + 1)
+      self.dfs(board, row, column - 1)     
