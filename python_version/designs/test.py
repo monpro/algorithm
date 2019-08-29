4c2df8572873b1f4564ac8cf5f68d492eca86b42
monpro <monproshen@gmail.com>
2019-08-29 16:28:31 +1000

    add the api design of twitter

---
+++ python_version/designs/test.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sodapy import Socrata
+
+# Unauthenticated client only works with public data sets. Note 'None'
+# in place of application token, and no username or password:
+# Example authenticated client (needed for non-public datasets):
+client = Socrata("data.cityofnewyork.us",
+                 app_token="auoZNB0hy6vhbqxl1rl6x9omb")
+
+# First 2000 results, returned as JSON from API / converted to Python list of
+# dictionaries by sodapy.
+results = client.get("8h9b-rp9u", limit=2000)
+
+# Convert to pandas DataFrame
+results_df = pd.DataFrame.from_records(results)
+
+print(results_df)
