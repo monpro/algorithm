a923ec0a506e267358241edf7096908fb45e809e
monpro <monproshen@gmail.com>
2019-09-08 16:10:42 +1000

    use dfa tricks to solve valid number

---
+++ python_version/strings/valid-number-65.py
@@ -0,0 +1,36 @@
+class Solution(object):
+  def isNumber(self, s):
+      """
+      :type s: str
+      :rtype: bool
+      """
+      #define a DFA
+      state = [{}, 
+              {'blank': 1, 'sign': 2, 'digit':3, '.':4}, 
+              {'digit':3, '.':4},
+              {'digit':3, '.':5, 'e':6, 'blank':9},
+              {'digit':5},
+              {'digit':5, 'e':6, 'blank':9},
+              {'sign':7, 'digit':8},
+              {'digit':8},
+              {'digit':8, 'blank':9},
+              {'blank':9}]
+      currentState = 1
+      for c in s:
+          if c >= '0' and c <= '9':
+              c = 'digit'
+          if c == ' ':
+              c = 'blank'
+          if c in ['+', '-']:
+              c = 'sign'
+          if c not in state[currentState]:
+              return False
+          currentState = state[currentState][c]
+      if currentState not in [3,5,8,9]:
+          return False
+      return True
+
+
+
+if __main__ == "__name__":
+    l = Solution()
