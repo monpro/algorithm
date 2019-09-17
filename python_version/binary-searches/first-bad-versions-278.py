bfb531ed8cba3d72eb211e02a030300f550973f6
monpro <monproshen@gmail.com>
2019-09-17 16:01:31 +1000

    majory element

---
+++ python_version/binary-searches/first-bad-versions-278.py
@@ -0,0 +1,19 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        while left < right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+              right = mid
+            else:
+              left = mid + 1
+        return left
