c0951aba3d5706908dea3a3f50cbf98d856ddd7a
monpro <monproshen@gmail.com>
2019-09-18 15:22:35 +1000

    lt 171 & 573

--- python_version/strings/excel-sheet-column-number-171.py
+++ python_version/strings/excel-sheet-column-number-171.py
@@ -1,4 +1,14 @@
 class Solution:
     def titleToNumber(self, s):
-      
+      if len(s) == 0:
+          return 0
+      result = 0
+      for i in range(len(s) - 1, -1, -1):
+          digit = ord(s[i]) - 65 + 1
+          result += 26 ** (len(s) - i - 1) * digit
+      return result
+
+if __name__ == "__main__":
+    l = Solution()
+    l.titleToNumber("A")
         
