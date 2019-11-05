fadbbfd47796cc8cc8f802fd920f05d7445c8d80
monpro <monproshen@gmail.com>
2019-11-05 15:44:37 +1100

    update major element using Boyer-Moore algorithm

--- python_version/arrays/majority-element-ii-229.py
+++ python_version/arrays/majority-element-ii-229.py
@@ -8,16 +8,15 @@ class Solution(object):
           return []
         candidate_a, candidate_b, count_a, count_b = 0, 1, 0, 0
         for num in nums:
-          if count_a == 0:
-            candidate_a, count_a = num, 1
-          elif count_b == 0:
-            candidate_b, count_b = num, 1
-          elif num == candidate_a:
+          if num == candidate_a:
             count_a += 1
           elif num == candidate_b:
             count_b += 1
+          elif count_a == 0:
+            candidate_a, count_a = num, 1
+          elif count_b == 0:
+            candidate_b, count_b = num, 1
           else:
             count_a -= 1
             count_b -= 1
-        result = [i for i in [candidate_a, candidate_b] if nums.count(i) > len(nums) // 3]
-        return list(set(result))
+        return [i for i in [candidate_a, candidate_b] if nums.count(i) > len(nums) // 3]
