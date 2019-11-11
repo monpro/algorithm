7f2b56c0bba13de5ba2d7b11bf429937eaf4c9a2
monpro <monproshen@gmail.com>
2019-11-11 11:22:58 +1100

    add bfs solution to find shortest path

--- python_version/dps/perfect-squares-279.py
+++ python_version/dps/perfect-squares-279.py
@@ -14,3 +14,27 @@ class Solution(object):
             dp[i] = min(dp[i], dp[i - j * j] + 1)
             j += 1
         return dp[-1]
+      
+    def numSquaresBfs(self, n):
+      if n == 0:
+        return 0
+      i = 1
+      squareList = []
+      while i * i <= n:
+        squareList.append(i * i)
+        i += 1
+      result = 0
+      toCheck = set(n)
+      while toCheck:
+        result += 1
+        temp = set()
+        for number in toCheck:
+          for square in squareList:
+            if number == square:
+              return result
+            elif number < square:
+              break
+            else:
+              temp.add(number - square)
+        toCheck = temp
+      return result
