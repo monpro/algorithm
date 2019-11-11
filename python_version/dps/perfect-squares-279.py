88278f4d3bf3a6eeb253133e802fb73705fc4461
monpro <monproshen@gmail.com>
2019-11-11 11:11:58 +1100

    add dp solution of perfect squares

--- python_version/dps/perfect-squares-279.py
+++ python_version/dps/perfect-squares-279.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import math
 class Solution:
     def numSquares(self, n):
@@ -12,3 +13,21 @@ class Solution:
         return dp[-1]
         
 
+=======
+class Solution(object):
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+          return 0
+        dp = [i for i in range(n + 1)]
+        dp[0] = 0
+        for i in range(1, n + 1):
+          j = 0
+          while j * j <= i:
+            dp[i] = min(dp[i], dp[i - j * j] + 1)
+            j += 1
+        return dp[-1]
+>>>>>>> 64589e8... add dp solution of perfect squares
