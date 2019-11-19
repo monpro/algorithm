42913b4ba826c91203e157f44ed3b0d1a25257a7
monpro <monproshen@gmail.com>
2019-11-19 18:34:18 +1100

    use slide window to handle the concentation

--- python_version/strings/substring-with-concatenation-of-all-words-30.py
+++ python_version/strings/substring-with-concatenation-of-all-words-30.py
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+=======
+import collections
+
+>>>>>>> 664fe46... use slide window to handle the concentation
 class Solution(object):
     def findSubstring(self, s, words):
         """
@@ -5,6 +10,7 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[int]
         """
+<<<<<<< HEAD
         length = len(words[0])
         wordMap = dict()
         wordList = [s[(i - 1) * length: i * length] for i in range(1, len(s) // length)]
@@ -57,3 +63,21 @@ if __name__ == "__main__":
 
   
         
+=======
+        if not words:
+          return []
+        k = len(words[0])
+        result = []
+        for left in range(k):
+          d = collections.Counter(words)
+          for right in range(left + k, len(s) + 1, k):
+            word = s[right - k: right]
+            d[word] -= 1
+            while d[word] < 0:
+              d[s[left: left + k]] += 1
+              left += k
+            if left + k * len(words) == right:
+              result.append(left)
+        return result
+            
+>>>>>>> 664fe46... use slide window to handle the concentation
