0f1e731cc458e59098c7dd2c7a4d0be3610cfeab
monpro <monproshen@gmail.com>
2019-12-05 16:20:42 +1100

    add memo to speed up

--- python_version/dps/concatenated-words-472.py
+++ python_version/dps/concatenated-words-472.py
@@ -9,22 +9,27 @@ class Solution(object):
         
         result = []
         for word in words:
-          if self.helper(word, words):
+          if self.helper(word, words, {}):
             result.append(word)
         return result
     
-    def helper(self, word, words):
+    def helper(self, word, words, memo):
+      if memo[word]:
+        return memo[word]
       for i in range(1, len(word)):
         prefix = word[0: i]
         suffix = word[i: ]
 
         if prefix in words and suffix in words:
+          memo[word] = True
           return True
         
         if prefix in words and self.helper(suffix, words):
+          memo[word] = True
           return True
         
         if suffix in words and self.helper(prefix, words):
+          memo[word] = True
           return True
       return False
 
