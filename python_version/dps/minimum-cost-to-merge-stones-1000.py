551b0f567fcd1925853a09f6041618f8d9da5780
monpro <monproshen@gmail.com>
2019-12-06 15:14:55 +1100

    modify the 3 layer dp helper method

--- python_version/dps/minimum-cost-to-merge-stones-1000.py
+++ python_version/dps/minimum-cost-to-merge-stones-1000.py
@@ -6,44 +6,50 @@ class Solution(object):
         :rtype: int
         """
         n = len(stones)
-        if n <= 1:
-          return False
+        if n == 1:
+          return 0
 
         prefix = [0] * (n + 1)
         memo = [[[0 for _ in range(K + 1)] for _ in range(n + 1)] for _ in range(n + 1)]
         for i in range(n):
           prefix[i + 1] = prefix[i] + stones[i]
-        def helper(i, j, targetPiles):
-          if memo[i][j][targetPiles] != 0:
-            return memo[i][j][targetPiles]
-          
-          if targetPiles == 1:
-            subMinCost = helper(i, j, K)
-            if subMinCost != float('Inf'):
-              memo[i][j][targetPiles] = prefix[j] - prefix[i - 1] + subMinCost
-            else:
-              memo[i][j][targetPiles] = subMinCost
-            return memo[i][j][targetPiles]
-          
-          minCost = float('Inf')
-
-          for k in range(i, j):
-            leftCost = helper(i, k, targetPiles - 1)
-            if leftCost == float("Inf"):
-              continue
-            rightCost = helper(k + 1, j, 1)
-            if rightCost == float("Inf"):
-              continue
-            minCost = min(leftCost + rightCost, minCost)
-          memo[i][j][targetPiles] = minCost
-          print(memo)
-          return minCost
-
-        result = helper(1, n, 1)
+        result = self.helper(1, n, 1, K, prefix, memo)
         if result == float("Inf"):
           return -1
         else:
           return result
+    def helper(self,i, j, targetPiles, K, prefix, memo):
+        if memo[i][j][targetPiles] != 0:
+          return memo[i][j][targetPiles]
+        if j - i  + 1 < targetPiles:
+          return float("Inf")
+        
+        if i == j:
+          if targetPiles == 1:
+            return 0
+          else:
+            return float("Inf")
+        if targetPiles == 1:
+          subMinCost = self.helper(i, j, K, K, prefix, memo)
+          if subMinCost != float('Inf'):
+            memo[i][j][targetPiles] = prefix[j] - prefix[i - 1] + subMinCost
+          else:
+            memo[i][j][targetPiles] = subMinCost
+          return memo[i][j][targetPiles]
+        
+        minCost = float('Inf')
+
+        for k in range(i, j):
+          leftCost = self.helper(i, k, targetPiles - 1, K, prefix, memo)
+          if leftCost == float("Inf"):
+            continue
+          rightCost = self.helper(k + 1, j, 1, K, prefix, memo)
+          if rightCost == float("Inf"):
+            continue
+          minCost = min(leftCost + rightCost, minCost)
+        memo[i][j][targetPiles] = minCost
+        return minCost
+        
        
           
       
