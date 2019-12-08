ea971ceb8f21cba856a0ffe7be64b59d0eef0b4d
monpro <monproshen@gmail.com>
2019-12-09 10:59:46 +1100

    add itera solution

--- python_version/binary-trees/sum-of-left-leaves-404.py
+++ python_version/binary-trees/sum-of-left-leaves-404.py
@@ -21,3 +21,22 @@ class Solution(object):
         return node.val
       
       return self.helper(node.left, True) + self.helper(node.right, False)
+
+    def sumOfLeftLeavesItera(self, root):
+      if not root:
+        return 0
+      stack = [root]
+      result = 0
+      while len(stack) > 0:
+        node = stack.pop()
+        if node:
+          if node.left:
+            if not node.left.left and not node.left.right:
+              result += node.left.val
+            else:
+              stack.append(node.left)
+          
+          if node.right:
+            if node.right.left or node.right.right:
+              stack.append(node.right)
+      return result
