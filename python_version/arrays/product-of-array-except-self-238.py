3a6801b1c5a4e3ad65e0a17d25bf24c1a0a717c8
monpro <monproshen@gmail.com>
2019-12-15 14:15:22 +1100

    divide multiple into left * Right

--- python_version/arrays/product-of-array-except-self-238.py
+++ python_version/arrays/product-of-array-except-self-238.py
@@ -4,5 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        result = []
-        return result
+        #       [2, 3, 4, 5]
+        # left: [1, 2, 2*3, 2*3*4]
+        # right:[3*4*5, 4*5, 5, 1]
+
+        p = 1
+        n = len(nums)
+        left = []
+        for i in range(n):
+            left.append(p)
+            p *= nums[i]
+        
+        p = 1
+        for i in range(n - 1, -1, -1):
+            left[i] *= p
+            p *= nums[i]
+
+        return left
+
+if __name__ == "__main__":
+    l = Solution()
+    print(l.productExceptSelf([1, 2, 3, 5]))
