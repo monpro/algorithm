81de872169248ac5606ff5135ebc4fb9ffcd6db5
monpro <monproshen@gmail.com>
2020-01-12 12:47:02 +1100

    reduce o(n^2) to o(n)

--- python_version/dps/longest-palindromic-subsequence-516.py
+++ python_version/dps/longest-palindromic-subsequence-516.py
@@ -10,3 +10,19 @@ class Solution:
                 else:
                     dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
         return dp[0][n - 1]
+
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
+        dp = [0 for _ in range(n)]
+        for i in range(n - 1, -1, -1):
+            dp[i] = 1
+            preVal = 0
+            for j in range(i + 1, n):
+                tempVal = dp[j]
+                if s[i] == s[j]:
+                    dp[j] = preVal + 2
+                else:
+                    dp[j] = max(dp[j], dp[j - 1])
+                preVal = tempVal
+        return dp[n - 1]
+
