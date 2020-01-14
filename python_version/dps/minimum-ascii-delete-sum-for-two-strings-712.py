34517ae5c3a918ef00fe0e3272b84cb116274eaa
monpro <monproshen@gmail.com>
2020-01-14 16:21:18 +1100

    optimal solution

--- python_version/dps/minimum-ascii-delete-sum-for-two-strings-712.py
+++ python_version/dps/minimum-ascii-delete-sum-for-two-strings-712.py
@@ -21,3 +21,20 @@ class Solution(object):
                     dps[i][j] = min(dps[i - 1][j] + ord(s1[i - 1]), dps[i][j - 1] + ord(s2[j - 1]))
         
         return dps[m][n]
+
+    def minimumDeleteSumOptimal(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: int
+        """
+        m, n = len(s1), len(s2)
+        dps = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        for i in range(1,m + 1):
+            for j in range(1, n + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dps[i][j] = dps[i - 1][j - 1] + ord(s[i - 1])
+                else:
+                    dps[i][j] = max(dps[i - 1][j] , dps[i][j - 1])
+        
+        return sum(map(ord, s1 + s2)) - 2 * dps[m][n]
