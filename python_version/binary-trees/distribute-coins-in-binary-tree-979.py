ca34e6532088c28f8254e912fb49e0e384d4e7a4
monpro <monproshen@gmail.com>
2020-02-02 16:56:41 +1100

    remove global variable and helper method

--- python_version/binary-trees/distribute-coins-in-binary-tree-979.py
+++ python_version/binary-trees/distribute-coins-in-binary-tree-979.py
@@ -20,3 +20,17 @@ class Solution:
         
         dfs(root)
         return self.result
+
+    def distributeCoins(self, root: TreeNode) -> int:
+        result = 0
+        if not root:
+            return 0
+        if root.left:
+            result += self.distributeCoins(root.left)
+            result += abs(root.left.val - 1)
+            root.val += root.left.val - 1
+        if root.right:
+            result += self.distributeCoins(root.right)
+            result += abs(root.right.val - 1)
+            root.val += root.right.val - 1
+        return result
