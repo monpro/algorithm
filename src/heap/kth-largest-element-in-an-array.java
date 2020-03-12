dc58ca9a3962b8301f90235bc8c629850c79d4ae
monpro <monproshen@gmail.com>
2020-03-12 23:06:16 +1100

    dfs flood fill

--- src/heap/kth-largest-element-in-an-array.java
+++ src/heap/kth-largest-element-in-an-array.java
@@ -1,7 +1,6 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> queue = new PriorityQueue<>();
-        int count = 0;
         for(int i: nums){
             queue.offer(i);
             if(queue.size()  > k)
