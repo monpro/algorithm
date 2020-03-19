0666faac996628f88e36e6c7951ae0ad57b21cd0
monpro <monproshen@gmail.com>
2020-03-19 22:11:01 +1100

    use 2 variables to optimise dp

--- src/dps/wiggle-subsequence.370.java
+++ src/dps/wiggle-subsequence.370.java
@@ -25,5 +25,22 @@ class Solution {
         return Math.max(up[nums.length - 1], down[nums.length - 1]);
         
     }
+    public int wiggleMaxLengthOptimiseSpace(int[] nums) {
+        if(nums.length == 0)
+            return 0;
+        
+        int up = 1, down = 1;
+        
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] > nums[i - 1]){
+                up = down + 1;
+            }
+            else if(nums[i] < nums[i - 1]){
+                down = up + 1;
+            }
+        }
+        
+        return Math.max(up, down);
 
+    }
 }
