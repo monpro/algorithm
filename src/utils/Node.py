80e30687405744fa902868444320d6e911d761fc
monpro <monproshen@gmail.com>
2020-04-24 21:27:53 +1000

    change structure

--- src/utils/Node.py
+++
@@ -1,9 +0,0 @@
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-class RandomListNode:
-    def __init__(self, x):
-        self.label = x
-        self.next = None
-        self.random = None
