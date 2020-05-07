09891913f2d6c76c1d67b318e5ef3e454ada4369
monpro <monproshen@gmail.com>
2020-05-07 22:43:12 +1000

    intert interval

---
+++ src/history/merge intervals/merge-intervals.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if(intervals.length < 2){
+            return intervals;
+        }
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        
+        List<int[]> result = new ArrayList<>();
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        
+        for(int i = 1; i < intervals.length; i++){
+            int[] interval = intervals[i];
+            if(interval[0] <= end){
+                end = Math.max(interval[1], end);
+            }else{
+                result.add(new int[]{start, end});
+                start = interval[0];
+                end = interval[1];
+            }
+        }
+        result.add(new int[]{start, end});
+        return result.toArray(new int[result.size()][]);
+    }
+}
