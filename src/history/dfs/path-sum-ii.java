6032db36855aac2a3a7fe50cefb73a82a1360f5b
monpro <monproshen@gmail.com>
2020-05-14 22:54:10 +1000

    dump path-sum py

---
+++ src/history/dfs/path-sum-ii.java
@@ -0,0 +1,39 @@
+package history.dfs;
+
+import java.util.*;
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+};
+
+class FindAllTreePaths {
+    public static List<List<Integer>> findPaths(TreeNode root, int sum) {
+      List<List<Integer>> allPaths = new ArrayList<>();
+      List<Integer> current = new ArrayList<>();
+  
+      dfs(allPaths, current, root, sum);
+      return allPaths;
+    }
+  
+    public static void dfs(List<List<Integer>> allPaths, List<Integer> currentPath, TreeNode currentNode, int sum){
+      if(currentNode == null){
+        return;
+      }
+      currentPath.add(currentNode.val);
+  
+      if(currentNode.val == sum && currentNode.left == null && currentNode.right == null){
+        allPaths.add(new ArrayList<>(currentPath));
+      }else{
+        dfs(allPaths, currentPath, currentNode.left, sum - currentNode.val);
+        dfs(allPaths, currentPath, currentNode.right, sum - currentNode.val);
+      }
+  
+      currentPath.remove(currentPath.size() - 1);
+    }
+}
