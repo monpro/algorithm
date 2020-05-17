d9c545fa41c730cd8144e0014af4649fd8645331
monpro <monproshen@gmail.com>
2020-05-17 18:43:08 +1000

    dump bst permutation

---
+++ src/history/permutation/unique-binary-search-trees-ii.java
@@ -0,0 +1,33 @@
+package history.permutation;
+
+import java.util.*;
+
+class EvaluateExpression {
+  public static List<Integer> diffWaysToEvaluateExpression(String input) {
+    List<Integer> result = new ArrayList<>();
+    // base case: if the input string is a number, parse and add it to output.
+    if (!input.contains("+") && !input.contains("-") && !input.contains("*")) {
+      result.add(Integer.parseInt(input));
+    } else {
+      for (int i = 0; i < input.length(); i++) {
+        char chr = input.charAt(i);
+        if (!Character.isDigit(chr)) {
+          // break the equation here into two parts and make recursively calls
+          List<Integer> leftParts = diffWaysToEvaluateExpression(input.substring(0, i));
+          List<Integer> rightParts = diffWaysToEvaluateExpression(input.substring(i + 1));
+          for (int part1 : leftParts) {
+            for (int part2 : rightParts) {
+              if (chr == '+')
+                result.add(part1 + part2);
+              else if (chr == '-')
+                result.add(part1 - part2);
+              else if (chr == '*')
+                result.add(part1 * part2);
+            }
+          }
+        }
+      }
+    }
+    return result;
+  }
+}
