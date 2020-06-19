0656cfd5c8bb92cfe61bb8a01705486813fd4977
monpro <monproshen@gmail.com>
2020-06-19 18:15:30 +1000

    shift group strings together

---
+++ src/strings/group-shifted-strings.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<String>> groupStrings(String[] strings) {
+        Map<String, List<String>> group = new HashMap<>();
+        
+        if(strings.length == 0){
+            return new ArrayList<>();
+        }
+        
+        for(String string: strings){
+            char base = string.charAt(0);
+            int offset = base - 'a';
+            
+            StringBuilder build = new StringBuilder();
+            for(int i = 0; i < string.length(); i++){
+                char ch = string.charAt(i);
+                ch = (char)(ch - offset);
+                if(ch < 'a'){
+                    ch += 26;
+                }
+                build.append(ch);
+            }
+            String key = build.toString();
+            group.putIfAbsent(key, new ArrayList<>());
+            group.get(key).add(string);
+        }
+        
+       return new ArrayList<>(group.values());
+        
+    }
+}
