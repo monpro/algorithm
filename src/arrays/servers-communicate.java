87ad149fdfcf979851be74557908877e72c52b8a
monpro <monproshen@gmail.com>
2020-06-25 21:45:08 +1000

    reduce from total to count valid num

---
+++ src/arrays/servers-communicate.java
@@ -0,0 +1,21 @@
+package arrays;
+
+class Solution {
+    public int minAddToMakeValid(String S) {
+        int leftBracket = 0, rightBracket = 0;
+        for(char ch: S.toCharArray()){
+            if(ch == '('){
+                rightBracket += 1;
+            }else if(ch == ')'){
+                if(rightBracket == 0){
+                    leftBracket += 1;
+                }else{
+                    rightBracket -= 1;
+                }
+            }
+        }
+        
+        return leftBracket + rightBracket;
+        
+    }
+}
