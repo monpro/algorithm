2d0b9aa8d36af7e287a2831ac3e221b1c2276b3a
monpro <monproshen@gmail.com>
2020-06-27 18:17:35 +1000

    fizz buzz string

---
+++ src/strings/fizzbuzz.java
@@ -0,0 +1,22 @@
+package strings;
+
+import java.util.*;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            if(i % 5 == 0 && i % 3 == 0){
+                result.add("FizzBuzz");
+            }else if(i % 5 == 0){
+                result.add("Buzz");
+            }else if(i % 3 == 0){
+                result.add("Fizz");
+            }else{
+                result.add(String.valueOf(i));
+            }
+        }
+        
+        return result;
+    }
+}
