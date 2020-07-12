5266e7635f7f146a7c7ffe6f8461a85d073dc124
monpro <monproshen@gmail.com>
2020-07-12 19:28:43 +1000

    use greedy algo to connect sticks to min cost

---
+++ src/heap/conneect-sticks.java
@@ -0,0 +1,21 @@
+package heap;
+
+import java.util.*;
+
+class Solution {
+    public int connectSticks(int[] sticks) {
+        Queue<Integer> queue = new PriorityQueue<>();
+        
+        for(int num: sticks){
+            queue.offer(num);
+        }
+        
+        int result = 0;
+        while(queue.size() > 1){
+            int first = queue.poll(), second = queue.poll();
+            result += first + second;
+            queue.offer(first + second);
+        }
+        
+        return result;
+    }
